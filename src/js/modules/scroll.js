2fabd8f0a2d1c693d287e665005afac243d5051a
Anna <a.kucheryavenkova@m-games-ltd.com>
2022-11-23 11:48:43 +0300

    add services block

--- src/js/modules/scroll.js
+++ src/js/modules/scroll.js
@@ -1,16 +1,22 @@
 module.exports = () => {
     const headerScroll = document.querySelector('.header_middle');
+    const menuLink = headerScroll.querySelectorAll('.desktop-menu__link');
 
     window.onscroll = function () {
         scrollPage();
     }
 
-
     function scrollPage() {
         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
             headerScroll.classList.add('_scroll');
+            menuLink.forEach(link => {
+                link.classList.add('_scroll');
+            });
         } else {
             headerScroll.classList.remove('_scroll');
+            menuLink.forEach(link => {
+                link.classList.remove('_scroll');
+            });
         }
     }
 }
